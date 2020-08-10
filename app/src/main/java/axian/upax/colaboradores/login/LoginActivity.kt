684782c3fc514758b471f09fc5184ec8b85e11ef
Norman SN <normansanchezn@gmail.com>
2020-08-10 18:27:19 -0500

    ***Feature: Add Firebase to project

---
+++ app/src/main/java/axian/upax/colaboradores/login/LoginActivity.kt
@@ -0,0 +1,17 @@
+package axian.upax.colaboradores.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import axian.upax.colaboradores.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        iv_button_auth.setOnClickListener {
+            
+        }
+    }
+}
