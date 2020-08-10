c60b3204b42be99bff89074052a2835691589705
Norman SN <normansanchezn@gmail.com>
2020-08-10 17:28:02 -0500

    ***Feature: Initial project

---
+++ app/src/main/java/axian/upax/colaboradores/MainActivity.kt
@@ -0,0 +1,11 @@
+package axian.upax.colaboradores
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
