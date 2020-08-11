831654e16eda6a2aab59cc31ee5599dce16454df
Norman SN <normansanchezn@gmail.com>
2020-08-10 23:18:13 -0500

    *Fix: Auth

--- app/src/main/java/axian/upax/colaboradores/home/Home.kt
+++ app/src/main/java/axian/upax/colaboradores/home/Home.kt
@@ -6,28 +6,22 @@ import android.os.Bundle
 import axian.upax.colaboradores.R
 import axian.upax.colaboradores.home.add_collaborator.AddCollaboratorActivity
 import axian.upax.colaboradores.home.show_collaborators.ShowCollaboratorsActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
-
-    private var nameUser: String? = null
-    private var email: String? = null
-    private var phoneNumber: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        nameUser = intent.extras?.getString("nameUser")
-        email = intent.extras?.getString("email")
-        phoneNumber = intent.extras?.getString("phoneNumber")
-        
-        btn_agregar_colaborador.setOnClickListener {
-            startActivity(Intent(this, AddCollaboratorActivity::class.java))
-        }
+        val signInAccount:GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this.applicationContext)
+        if (signInAccount != null)
+            tvCurrentName.text = signInAccount.displayName
 
-        btn_mis_colaboradores.setOnClickListener {
-            startActivity(Intent(this, ShowCollaboratorsActivity::class.java))
-        }
+        btn_agregar_colaborador.setOnClickListener { startActivity(Intent(this, AddCollaboratorActivity::class.java)) }
+
+        btn_mis_colaboradores.setOnClickListener { startActivity(Intent(this, ShowCollaboratorsActivity::class.java)) }
     }
 }
--- app/src/main/java/axian/upax/colaboradores/login/LoginActivity.kt
+++ app/src/main/java/axian/upax/colaboradores/login/LoginActivity.kt
@@ -3,55 +3,90 @@ package axian.upax.colaboradores.login
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import axian.upax.colaboradores.R
 import axian.upax.colaboradores.home.Home
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : AppCompatActivity() {
 
-    private var RC_SIGN_IN: Int = 10001
+    private var auth: FirebaseAuth? = null
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+    private val RC_SIGN_IN: Int = 123
+    private val TARGET = "debug"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.GoogleBuilder().build())
+        auth = FirebaseAuth.getInstance()
 
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(),
-            RC_SIGN_IN)
+        createRequest()
 
+        iv_button_auth.setOnClickListener { signIn() }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth?.currentUser
+
+        if(currentUser!=null)
+            startActivity(Intent(this, Home::class.java))
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
         if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
-
-            if (resultCode == Activity.RESULT_OK) {
-                val user = FirebaseAuth.getInstance().currentUser
-                val homeIntent = Intent(this, Home::class.java)
-                homeIntent.putExtra("email", response?.email)
-                homeIntent.putExtra("phoneNumber", response?.phoneNumber)
-                homeIntent.putExtra("nameUser", user?.displayName)
-                Toast.makeText(this, "$user", Toast.LENGTH_LONG).show()
-                startActivity(homeIntent)
-            } else{
-                Toast.makeText(this, "Hubo un error en la autenticación.", Toast.LENGTH_SHORT).show()
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TARGET, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.w(TARGET, "Google sign in failed", e)
             }
-        } else {
-            Toast.makeText(this, "No se pudo crear la conexión con tu cuenta de Google.", Toast.LENGTH_SHORT).show()
         }
     }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth?.currentUser
+                    Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, Home::class.java))
+                } else {
+                    Toast.makeText(this, "La autenticación falló.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun createRequest() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
 }
